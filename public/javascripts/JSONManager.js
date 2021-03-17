36431e566e26c87ae87329fe136d4c3c565038b4
damilog <dami.k.dev@gmail.com>
2021-03-17 18:25:47 +0900

    stash에서 저장된거 가져와서 커밋..

--- public/javascripts/JSONManager.js
+++ public/javascripts/JSONManager.js
@@ -2,12 +2,33 @@ import { _ } from "./util.js";
 export default class JSONManager {
   constructor() {
     this.server = "http://localhost:3000";
+    this.stationList;
   }
 
-  async request(url) {
-    const response = await fetch(`${this.server}/${url}`);
-    console.log(response.json());
+  requestData(url) {
+    return fetch(`${this.server}/${url}`).then(res => res.json());
   }
 
-  parseBySubwayLines(data) {}
+  parseByLines(data) {
+    const stationList = {};
+
+    //이 코드점 살려주세요..-----
+    data.forEach(x => {
+      const currentLine = x["line_num"];
+      stationList[currentLine] = [];
+    });
+
+    data.forEach(x => {
+      const currentLine = x["line_num"];
+      const currentStation = x["station_nm"];
+      stationList[currentLine].push(currentStation);
+    });
+    //이 코드점 살려주세요..----
+
+    return stationList;
+  }
 }
+// 내가 원하는 형태
+// const obj = {01호선 : [시청, 서울역...],
+// 02호선 :[시청, 신드림...]
+// } -> 이렇게 되면 호선별로 역이 몇개있는지 파악가능.
--- public/javascripts/WaitingRoomUI.js
+++ public/javascripts/WaitingRoomUI.js
@@ -1,16 +1,21 @@
 import { _ } from "./util.js";
 import RenderManager from "./RenderManager.js";
 import SocketManager from "./SocketManager.js";
+import JSONManager from "./JSONManager.js";
 
 export default class WaitingRoomUI {
   constructor(boardContainer) {
     this.$boardContainer = boardContainer;
     this.renderManager = new RenderManager(this.$boardContainer);
+    this.jsonManager = new JSONManager();
+    this.stationListByLines;
+    this.lineSize;
     this.init();
   }
 
   init() {
     this.onEvent();
+    this.getStationListByLines();
   }
 
   onEvent() {
@@ -20,11 +25,24 @@ export default class WaitingRoomUI {
     );
   }
 
-  requestJSONData() {}
+  async getStationListByLines() {
+    const jsonData = await this.jsonManager.requestData("lines");
+    this.stationListByLines = this.jsonManager.parseByLines(jsonData);
+    this.lineSize = Object.keys(this.stationListByLines).length;
+  }
+
+  drawLineInfoOnBtn() {
+    for (let i = 1; i <= this.lineSize; i++) {
+      const $currentBtnText = _.$(`#line${i}-text`);
+      const currentLineSize = this.stationListByLines[`0${i}호선`].length;
+      $currentBtnText.textContent = `${currentLineSize}개역 / 난이도 상`;
+    }
+  }
 
   drawWaitingRoom() {
     _.$Remove(".changeable-area");
     this.renderManager.renderAfterNav(this.makeTemplate());
+    this.drawLineInfoOnBtn.call(this);
   }
 
   makeTemplate() {
@@ -45,49 +63,49 @@ export default class WaitingRoomUI {
         </div>
         <div class="board-wrap__lines__li" id="line2">
           <span class="board-wrap__lines__li__title">2호선</span>
-          <span class="board-wrap__lines__li__text" id="line1-text"
+          <span class="board-wrap__lines__li__text" id="line2-text"
             >10개역 / 난이도 상</span
           >
         </div>
         <div class="board-wrap__lines__li" id="line3">
           <span class="board-wrap__lines__li__title">3호선</span>
-          <span class="board-wrap__lines__li__text" id="line1-text"
+          <span class="board-wrap__lines__li__text" id="line3-text"
             >10개역 / 난이도 상</span
           >
         </div>
         <div class="board-wrap__lines__li" id="line4">
           <span class="board-wrap__lines__li__title">4호선</span>
-          <span class="board-wrap__lines__li__text" id="line1-text"
+          <span class="board-wrap__lines__li__text" id="line4-text"
             >10개역 / 난이도 상</span
           >
         </div>
         <div class="board-wrap__lines__li" id="line5">
           <span class="board-wrap__lines__li__title">5호선</span>
-          <span class="board-wrap__lines__li__text" id="line1-text"
+          <span class="board-wrap__lines__li__text" id="line5-text"
             >10개역 / 난이도 상</span
           >
         </div>
         <div class="board-wrap__lines__li" id="line6">
           <span class="board-wrap__lines__li__title" id="line6">6호선</span>
-          <span class="board-wrap__lines__li__text" id="line1-text"
+          <span class="board-wrap__lines__li__text" id="line6-text"
             >10개역 / 난이도 상</span
           >
         </div>
         <div class="board-wrap__lines__li" id="line7">
           <span class="board-wrap__lines__li__title" id="line7">7호선</span>
-          <span class="board-wrap__lines__li__text" id="line1-text"
+          <span class="board-wrap__lines__li__text" id="line7-text"
             >10개역 / 난이도 상</span
           >
         </div>
         <div class="board-wrap__lines__li" id="line8">
           <span class="board-wrap__lines__li__title" id="line8">8호선</span>
-          <span class="board-wrap__lines__li__text" id="line1-text"
+          <span class="board-wrap__lines__li__text" id="line8-text"
             >10개역 / 난이도 상</span
           >
         </div>
         <div class="board-wrap__lines__li" id="line9">
           <span class="board-wrap__lines__li__title" id="line9">9호선</span>
-          <span class="board-wrap__lines__li__text" id="line1-text"
+          <span class="board-wrap__lines__li__text" id="line9-text"
             >10개역 / 난이도 상</span
           >
         </div>
